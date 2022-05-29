1619402c163c26b4698b6cc15933d7f344a8a34b
Kyntaz <1260pedroquintas@gmail.com>
2022-05-29 19:41:39 +0100

    Make contact form use mailto

--- src/init.js
+++ src/init.js
@@ -4,8 +4,24 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 })
 
+function sendEmail(subject, body) {
+    const params = new URLSearchParams({
+        subject,
+        body,
+    }).toString();
+    window.open(`mailto:ca.appassionata@gmail.com?${params}`)
+}
+
 function formSubmission() {
-    form = document.getElementById("contactForm");
-    form.submit();
-    form.reset();
+    const form = document.forms.contactForm;
+    const subject = `Pedido de Orçamento ${form.name.value}`;
+    const body =
+`Nome: ${form.name.value}
+Email: ${form.email.value}
+Data: ${form.date.value}
+Local: ${form.place.value}
+
+${form.message.value}`;
+
+    sendEmail(subject, body);
 }
